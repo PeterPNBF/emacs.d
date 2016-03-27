bf4b3279cab31de45c0a4c5395770e5b0e509b6c
PeterPNBF <yuhaimingbj@gmail.com>
2016-03-27 17:50:49 +0800

    inital import

---
+++ elpa/dash-20160306.1222/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22259 64925 661998 467000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
